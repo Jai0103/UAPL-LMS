0442b629f6d71aa921e1b672f6d7b5959f65f135
Jairus <orolazajairus@gmail.com>
2026-06-01 22:14:39 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {}
+    extend: {
+      boxShadow: {
+        premium: "0 24px 70px rgba(15, 23, 42, 0.16)"
+      }
+    }
   },
   plugins: []
 };
