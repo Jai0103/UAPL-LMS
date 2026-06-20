4a31a04e18be49a521ad78cc09dc61366b10a8e1
Jairus <orolazajairus@gmail.com>
2026-06-20 09:38:59 +0800

    Update courseNotes.js

--- src/data/courseNotes.js
+++ src/data/courseNotes.js
@@ -1,17 +1,14 @@
-/*
-  You are looking for your MotherFather!
-*/
 export const DEFAULT_COURSE_NOTES = [
     {
         id: "note-001",
         title: "UAPL Course Notes",
-        url: "You are looking for your MotherFather!",
+        url: "/UAPL-LMS/notes/UAPL TM4M5M6 2025 - PDF.pptx",
         createdAt: "2026-06-02"
     },
     {
         id: "note-002",
         title: "CAAS UA Regulations",
-        url: "You are looking for your MotherFather!",
+        url: "/UAPL-LMS/notes/UAPL TM1M2M3 2025 - PDF.pptx",
         createdAt: "2026-06-02"
     }
 ];
