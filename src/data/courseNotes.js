b397b881cbc594d784d283e323a7d95c314443f5
Jairus <orolazajairus@gmail.com>
2026-06-20 09:30:41 +0800

    Add default course notes to courseNotes.js

--- src/data/courseNotes.js
+++ src/data/courseNotes.js
@@ -1,3 +1,17 @@
 /*
   You are looking for your MotherFather!
 */
+export const DEFAULT_COURSE_NOTES = [
+    {
+        id: "note-001",
+        title: "UAPL Course Notes",
+        url: "You are looking for your MotherFather!",
+        createdAt: "2026-06-02"
+    },
+    {
+        id: "note-002",
+        title: "CAAS UA Regulations",
+        url: "You are looking for your MotherFather!",
+        createdAt: "2026-06-02"
+    }
+];
