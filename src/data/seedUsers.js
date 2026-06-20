7b5c0acf0a0c680d719009a6f726c9e03bf59df4
Jairus <orolazajairus@gmail.com>
2026-06-20 09:47:47 +0800

    Update seedUsers.js

--- src/data/seedUsers.js
+++ src/data/seedUsers.js
@@ -1,18 +1,18 @@
 export const DEFAULT_USERS = [
     {
         id: "admin-001",
-        name: "Jairus Admin",
-        username: "admin",
-        password: "admin123",
+        name: "Your MotherFater's Name",
+        username: "YouMotherFaher",
+        password: "1122123321",
         role: "admin",
         status: "Active",
         createdAt: "2026-01-01"
     },
     {
         id: "student-001",
-        name: "Student One",
-        username: "student",
-        password: "student123",
+        name: "Your MotherFater's Name",
+        username: "YouMotherFaher",
+        password: "1122123321",
         role: "student",
         status: "Active",
         createdAt: "2026-01-01"
