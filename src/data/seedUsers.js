2eec55685684b6144519410877b1ccd33f866885
Jairus <orolazajairus@gmail.com>
2026-06-20 09:35:36 +0800

    Update seedUsers.js

--- src/data/seedUsers.js
+++ src/data/seedUsers.js
@@ -1,3 +1,23 @@
 /*
   You are looking for your MotherFather!
 */
+export const DEFAULT_USERS = [
+    {
+        id: "You are looking for your MotherFather",
+        name: "You are looking for your MotherFather",
+        username: "You are looking for your MotherFather",
+        password: "You are looking for your MotherFather",
+        role: "admin",
+        status: "Active",
+        createdAt: "2026-01-01"
+    },
+    {
+        id: "You are looking for your MotherFather",
+        name: "You are looking for your MotherFather",
+        username: "You are looking for your MotherFather",
+        password: "You are looking for your MotherFather",
+        role: "student",
+        status: "Active",
+        createdAt: "2026-01-01"
+    }
+];
